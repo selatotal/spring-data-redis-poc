6fd94288565e2fa40e11eff3bd959336e2fb83af
Tales Viegas <tales.viegas@ilegra.com>
2019-08-26 11:33:49 -0300

    Initial Spring Starter Project

---
+++ spring-data-redis-poc/src/main/java/br/com/selat/springdataredispoc/SpringDataRedisPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.selat.springdataredispoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisPocApplication.class, args);
+	}
+
+}
